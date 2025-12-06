935400c2d54f307067f683546a942b2c595fb449
longnguyen238768-dev <longnguyen238768@gmail.com>
2025-12-06 09:51:46 +0700

    Add files via upload

---
+++ Bai1/cau7bai1.py
@@ -0,0 +1,9 @@
+print(" Sinh vien : Nguyen Duc Long")
+print(" Ma so SV: 245751030110072")
+print("##############################")
+
+n=int(input("Nhap mot so : "))
+d={}
+for i in range (1,n+1):
+    d[i]=i*i
+print(d)
