a5a55a903cc8fb9a27c44dcac07d7947560ed6cc
longnguyen238768-dev <longnguyen238768@gmail.com>
2025-12-06 10:57:54 +0700

    Add files via upload

---
+++ Bai2/cau5.2.py
@@ -0,0 +1,12 @@
+print(" Sinh vien : Nguyen Duc Long")
+print(" Ma so SV: 245751030110072")
+print("##############################")
+
+
+a='Hello Guy!'
+def say():
+    global a
+    a='Vinh University'
+    print(a)
+say()
+print(a)
