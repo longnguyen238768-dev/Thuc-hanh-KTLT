26e119d6025baa468c0d2dce8ea44e62c914b718
longnguyen238768-dev <longnguyen238768@gmail.com>
2025-12-06 17:11:25 +0700

    Add files via upload

---
+++ Bai3/cau16.3.py
@@ -0,0 +1,9 @@
+print(" Sinh vien : Nguyen Duc Long")
+print(" Ma so SV: 245751030110072")
+print("##############################")
+
+
+s = input("Nhập chuỗi các số nhị phân (cách bằng dấu phẩy): ")
+lst = s.split(",")
+for x in lst:
+    print(x)
