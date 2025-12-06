9c0fa159d053a99a76bd5516f2e20ac90acd411d
longnguyen238768-dev <longnguyen238768@gmail.com>
2025-12-06 17:08:18 +0700

    Add files via upload

---
+++ Bai3/cau15.3.py
@@ -0,0 +1,9 @@
+print(" Sinh vien : Nguyen Duc Long")
+print(" Ma so SV: 245751030110072")
+print("##############################")
+
+
+s = input("Nhập các từ tiếng Anh: ").split()
+s.sort()    
+for w in s:
+    print(w)
