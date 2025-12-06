6072f6d2b8862451a3fbda1fcb1840ed52feeae2
longnguyen238768-dev <longnguyen238768@gmail.com>
2025-12-06 17:04:29 +0700

    Add files via upload

---
+++ Bai3/cau13.3.py
@@ -0,0 +1,7 @@
+print(" Sinh vien : Nguyen Duc Long")
+print(" Ma so SV: 245751030110072")
+print("##############################")
+
+
+ds = input('Nhap chuoi: ').split()
+print("Vị trí của chuỗi 'abc' là:", ds.index('abc'))
