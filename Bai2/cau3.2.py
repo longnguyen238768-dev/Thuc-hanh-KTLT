84c5091e96e545964a77d32bf84eb57fe4503ef0
longnguyen238768-dev <longnguyen238768@gmail.com>
2025-12-06 10:51:49 +0700

    Add files via upload

---
+++ Bai2/cau3.2.py
@@ -0,0 +1,8 @@
+print(" Sinh vien : Nguyen Duc Long")
+print(" Ma so SV: 245751030110072")
+print("##############################")
+
+def say_hello():
+    a='Hello'
+    return a
+print(say_hello())
