9406ce11bafc69a5e8d01aa12d3a967498ae4cde
longnguyen238768-dev <longnguyen238768@gmail.com>
2025-12-06 16:40:10 +0700

    Add files via upload

---
+++ Bai3/cau2.3.py
@@ -0,0 +1,10 @@
+print(" Sinh vien : Nguyen Duc Long")
+print(" Ma so SV: 245751030110072")
+print("##############################")
+
+
+S = input("Nhap chuoi S: ")
+
+for ch in S:
+    if ch not in [' ', '\t']:
+        print(ch)
