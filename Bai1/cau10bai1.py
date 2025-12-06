334b92a9fd3d4342c64cb07bf1f46c6df0a839e7
longnguyen238768-dev <longnguyen238768@gmail.com>
2025-12-06 09:56:48 +0700

    Add files via upload

---
+++ Bai1/cau10bai1.py
@@ -0,0 +1,11 @@
+print(" Sinh vien : Nguyen Duc Long")
+print(" Ma so SV: 245751030110072")
+print("##############################")
+
+
+
+a='hi i am python programmer'
+b=a.split()
+print(b)
+c=' '.join(b)
+print(c)
