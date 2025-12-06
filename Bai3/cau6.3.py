5f4f5acd68d3ac67eb1f4b6b55965a9810dfac48
longnguyen238768-dev <longnguyen238768@gmail.com>
2025-12-06 16:51:19 +0700

    Add files via upload

---
+++ Bai3/cau6.3.py
@@ -0,0 +1,12 @@
+print(" Sinh vien : Nguyen Duc Long")
+print(" Ma so SV: 245751030110072")
+print("##############################")
+
+
+full_name = input("Nhập họ và tên: ").strip().split()
+
+ho = full_name[0]
+ten = full_name[1]
+
+print("Họ:", ho)
+print("Tên:", ten)
