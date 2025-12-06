86d0564c62d1098b797d1d64b9542a282d9764c7
longnguyen238768-dev <longnguyen238768@gmail.com>
2025-12-06 10:50:40 +0700

    Add files via upload

---
+++ Bai2/cau1.2.py
@@ -0,0 +1,9 @@
+print(" Sinh vien : Nguyen Duc Long")
+print(" Ma so SV: 245751030110072")
+print("##############################")
+
+
+def sum(a,b):
+    print('sum=' +str(a+b))
+sum(4,5) #tính tổng 2 số 4,5
+sum(3,7) #tính tổng 2 số 3,7
