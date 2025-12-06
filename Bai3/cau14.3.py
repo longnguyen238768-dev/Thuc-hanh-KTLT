a68c4626a079bd35417982bd9f82e9c1b6af7316
longnguyen238768-dev <longnguyen238768@gmail.com>
2025-12-06 17:05:55 +0700

    Add files via upload

---
+++ Bai3/cau14.3.py
@@ -0,0 +1,9 @@
+print(" Sinh vien : Nguyen Duc Long")
+print(" Ma so SV: 245751030110072")
+print("##############################")
+
+
+ds = input('Nhap chuoi: ').split()
+ds.sort()
+for ch in ds:
+    print(ch)
