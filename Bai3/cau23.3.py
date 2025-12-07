04f556ea29656bea9340af24c65e8f8612673e96
longnguyen238768-dev <longnguyen238768@gmail.com>
2025-12-07 15:59:32 +0700

    Add files via upload

---
+++ Bai3/cau23.3.py
@@ -0,0 +1,18 @@
+print(" Sinh vien : Nguyen Duc Long")
+print(" Ma so SV: 245751030110072")
+print("##############################")
+
+
+s = input("Nhập câu: ")
+
+so_chu_cai = 0
+so_chu_so = 0
+
+for ch in s:
+    if ch.isalpha():
+        so_chu_cai += 1
+    elif ch.isdigit():
+        so_chu_so += 1
+
+print("Số chữ cái là:", so_chu_cai)
+print("Số chữ số là:", so_chu_so)
