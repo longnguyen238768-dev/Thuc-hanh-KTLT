0fe21584f5c6cbefe777fcf24610781ac65221ff
longnguyen238768-dev <longnguyen238768@gmail.com>
2025-12-07 16:03:30 +0700

    Add files via upload

---
+++ Bai3/cau25.3.py
@@ -0,0 +1,12 @@
+print(" Sinh vien : Nguyen Duc Long")
+print(" Ma so SV: 245751030110072")
+print("##############################")
+
+
+s = input("Nhập danh sách số (cách nhau bởi dấu phẩy): ")
+
+nums = [int(x) for x in s.split(',')]
+
+odd = [str(x) for x in nums if x % 2 == 1]
+
+print(",".join(odd))
