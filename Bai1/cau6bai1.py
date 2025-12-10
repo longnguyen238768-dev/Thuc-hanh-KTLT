6755a17a728a3c5a5b37b1ef7243e2905b88ff8a
longnguyen238768-dev <longnguyen238768@gmail.com>
2025-12-10 15:56:31 +0700

    Update cau6bai1.py

--- Bai1/cau6bai1.py
+++ Bai1/cau6bai1.py
@@ -1,5 +1,11 @@
+print(" Sinh vien : Nguyen Duc Long")
+print(" Ma so SV: 245751030110072")
+print("##############################")
+
+
 nums=[]
 for i in range (2000,3201):
     if i % 7==0 and i%5!=0:
         nums.append(str(i))
 print(",".join(nums))
+
