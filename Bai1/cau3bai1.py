d326cdb7a97c32275d9e353c8f178abf9ab11c26
longnguyen238768-dev <longnguyen238768@gmail.com>
2025-12-10 15:55:45 +0700

    Update cau3bai1.py

--- Bai1/cau3bai1.py
+++ Bai1/cau3bai1.py
@@ -1,5 +1,11 @@
+print(" Sinh vien : Nguyen Duc Long")
+print(" Ma so SV: 245751030110072")
+print("##############################")
+
+
 n=int(input("Enter a number---->"))
 if n % 2 ==0:
     print("EVEN Number");
 else:
     print("ODD Number");
+
