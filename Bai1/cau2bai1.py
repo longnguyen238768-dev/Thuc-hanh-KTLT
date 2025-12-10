29cc7f5d5130c2acfc6e56e0a5f52171e51a7daf
longnguyen238768-dev <longnguyen238768@gmail.com>
2025-12-10 15:55:28 +0700

    Update cau2bai1.py

--- Bai1/cau2bai1.py
+++ Bai1/cau2bai1.py
@@ -1,3 +1,8 @@
+print(" Sinh vien : Nguyen Duc Long")
+print(" Ma so SV: 245751030110072")
+print("##############################")
+
+
 import math;
 x1=float(input("Enter x1--->"))
 y1=float(input("Enter y1--->"))
@@ -7,3 +12,4 @@ d1=(x2-x1)*(x2-x1);
 d2=(y2-y1)*(y2-y1);
 res=math.sqrt(d1+d2)
 print("Distance between two points:",res);
+
